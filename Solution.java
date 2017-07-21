e0df3233fceb76f077689fe72863ed2d489f7bd8
cjohnson73 <cjohnson73@murraystate.edu>
2017-07-21 10:59:14 -0400

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -244,8 +244,9 @@ public class Solution {
 			}
 			
 			solved = tsolved;
-			if(!changed)
+			if(!changed && !solved)
 			{
+				System.out.println("Unsolved");
 				solved = true;
 			}
         }
@@ -271,4 +272,14 @@ public class Solution {
 0 0 0 0 0 0 4 9 0
 7 0 6 0 1 0 0 0 0
 0 0 0 5 4 0 0 6 0
+
+8 0 0 0 0 0 0 0 0 
+0 0 3 6 0 0 0 0 0 
+0 7 0 0 9 0 2 0 0 
+0 5 0 0 0 7 0 0 0 
+0 0 0 0 4 5 7 0 0 
+0 0 0 1 0 0 0 3 0 
+0 0 1 0 0 0 0 6 8 
+0 0 8 5 0 0 0 1 0 
+0 9 0 0 0 0 4 0 0 
  */
